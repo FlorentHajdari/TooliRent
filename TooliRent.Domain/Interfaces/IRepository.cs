445f6abeeb90c581f03e303316e5b57b2dfd75ee
FlorentHajdari <florent.hajdari@hotmail.se>
2025-09-17 17:37:16 +0200

    generic repository interface

---
+++ TooliRent.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace TooliRent.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>>? predicate = null);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
+
