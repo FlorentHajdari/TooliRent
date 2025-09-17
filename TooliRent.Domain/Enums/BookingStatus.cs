aa8ee20b1801149f19676ba44b6c76e7258357b3
FlorentHajdari <florent.hajdari@hotmail.se>
2025-09-17 17:34:20 +0200

    fixed enums

---
+++ TooliRent.Domain/Enums/BookingStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooliRent.Domain.Enums
+{
+    public enum BookingStatus
+    {
+        Draft = 0,
+        Confirmed = 1,
+        PickedUp = 2,
+        Returned = 3,
+        Cancelled = 4,
+        Overdue = 5
+    }
+}
+
---
+++ TooliRent.Domain/Enums/ToolStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooliRent.Domain.Enums
+{
+    public enum ToolStatus
+    {
+        Available = 0,
+        Reserved = 1,
+        CheckedOut = 2,
+        Maintenance = 3,
+        Retired = 4
+    }
+}
+
